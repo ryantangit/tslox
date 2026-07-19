683352a4b7161c9f004ed9009ddb6aac027ec2da
Ryan Tan <ryantan.mail@gmail.com>
2026-07-19 02:53:01 -0700

    added prettier plugin to lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,19 @@
 import js from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig({
     files: ['**/*.{js,ts}'],
     extends: [
 	js.configs.recommended,
 	tseslint.configs.strictTypeChecked,
-	tseslint.configs.stylisticTypeChecked],
+	tseslint.configs.stylisticTypeChecked,
+	],
     languageOptions: {
 	parserOptions: {
 	    projectService: true
 	},
-    }
-});
+    },
+}, prettierRecommended);
+    
